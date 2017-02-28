31952ba3bf272f2ca6a95867b9b99d25d416d1c6
ikngtty <ikngtty@gmail.com>
2017-02-28 22:29:56 +0900

    Add the StringUtility.Repeat method.

---
+++ src/MyLibrary/Utility/StringUtility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Ikngtty.MyLibrary.Utility
+{
+	public static class StringUtility
+	{
+		public static string Repeat(this string text, uint repeatCount)
+		{
+			return string.Concat(Enumerable.Repeat(text, (int)repeatCount));
+		}
+	}
+}
---
+++ src/MyLibraryTest/Utility/StringUtilityTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using Ikngtty.MyLibrary.Utility;
+
+namespace Ikngtty.MyLibraryTest.Utility
+{
+	[TestFixture]
+	public class StringUtilityTest
+	{
+		[Test]
+		public void RepeatTest()
+		{
+			Assert.AreEqual("hogehogehoge", "hoge".Repeat(3));
+			Assert.AreEqual("", "hoge".Repeat(0));
+		}
+	}
+}
